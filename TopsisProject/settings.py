6ff1867a08fffd9687e781a6cd7e1115a5b76952
Prakhar Jindal <iamprakharjindal@gmail.com>
2020-11-13 22:06:47 +0530

    Update 2

--- TopsisProject/settings.py
+++ TopsisProject/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'TopsisProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -85,7 +85,7 @@ DATABASES = {
     }
 }
 
-
+DATA_UPLOAD_MAX_NUMBER_FIELDS = None
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
